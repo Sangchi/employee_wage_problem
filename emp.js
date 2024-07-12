7025d7f698b2ed9a9a27b6ce49ff274b6435afa1
Sangchi <prashnatchavan726386@gmail.com>
2024-07-12 16:19:43 +0530

    [prashant] add: third commit

--- emp.js
+++ emp.js
@@ -2,26 +2,25 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-function calculateDailyWage() {
-    const attendance = Math.floor(Math.random() * 3); 
-
-    let hoursWorked;
-
+function getWorkHours(attendance) {
     switch (attendance) {
         case 0: 
-            hoursWorked = 0;
-            break;
+            return 0;
         case 1: 
-            hoursWorked = PART_TIME_HOURS;
-            break;
+            return PART_TIME_HOURS;
         case 2: 
-            hoursWorked = FULL_TIME_HOURS;
-            break;
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
     }
+}
 
+function calculateDailyWage() {
+    const attendance = Math.floor(Math.random() * 3); 
+    const hoursWorked = getWorkHours(attendance);
     const dailyWage = hoursWorked * WAGE_PER_HOUR;
     return dailyWage;
 }
 
-const daily = calculateDailyWage();
-console.log(`Daily Wage: $${daily}`);
+const dailyWage = calculateDailyWage();
+console.log(`Daily Wage: $${dailyWage}`);
